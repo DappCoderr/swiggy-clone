f4bfc9b428d6900c1031d8c49b61f4a94a2ee311
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-03-10 13:36:32 +0530

    Add Filter logic in res list

--- web/src/components/RestaurentList.jsx
+++ web/src/components/RestaurentList.jsx
@@ -1,22 +1,34 @@
 import RestaurentCard from "./RestaurentCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
 
 const RestaurentList = () => {
+  const [listOfRes, setListOfRes] = useState(resObj);
+
   return (
     <div className="restaurent_container">
-      <form action="">
+      {/* <form action="">
         <input
           className="i_search"
           type="search"
           placeholder="search your fav food"
-        />
-        <button className="btn_search" type="submit">
-          Search
-        </button>
-      </form>
+        /> */}
+      <button
+        className="btn_search"
+        type="submit"
+        onClick={() => {
+          const filterData = listOfRes.filter(
+            (ele) => ele.info.avgRating >= 4.5
+          );
+          setListOfRes(filterData);
+        }}
+      >
+        Filter Restaurant
+      </button>
+      {/* </form> */}
       <h1 className="restaurent_heading">Top restaurant chains in Jodhpur</h1>
       <section className="restaurent_items">
-        {resObj.map((res, index) => {
+        {listOfRes.map((res, index) => {
           return <RestaurentCard key={index} resData={res} />;
         })}
       </section>
