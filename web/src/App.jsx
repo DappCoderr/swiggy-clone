ea7e24da943227ed66553badd804efa6acc59a2d
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-03-11 18:00:07 +0530

    Add: react-router-dom & create browser router

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,6 +2,11 @@
 import "./App.css";
 import Header from "./components/Header";
 import RestaurentList from "./components/RestaurentList";
+import { createBrowserRouter } from "react-router-dom";
+import Error from "./components/Error";
+import SignIn from "./components/SignIn";
+import Offer from "./components/Offer";
+import Help from "./components/Help";
 
 const App = () => {
   return (
@@ -12,4 +17,11 @@ const App = () => {
   );
 };
 
-export default App;
+const appRouter = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <Error /> },
+  { path: "/offer", element: <Offer /> },
+  { path: "/help", element: <Help /> },
+  { path: "/signIn", element: <SignIn /> },
+]);
+
+export default appRouter;
