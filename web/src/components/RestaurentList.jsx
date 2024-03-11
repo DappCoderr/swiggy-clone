605453d61e62d6c1ff7ecf2684e4cf6108359100
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-03-11 15:30:41 +0530

    Update: fetching data from swiggy live api

--- web/src/components/RestaurentList.jsx
+++ web/src/components/RestaurentList.jsx
@@ -1,18 +1,29 @@
 import RestaurentCard from "./RestaurentCard";
-import resObj from "../utils/mockData";
-import { useState } from "react";
+// import resObj from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 const RestaurentList = () => {
-  const [listOfRes, setListOfRes] = useState(resObj);
+  const [listOfRes, setListOfRes] = useState([]);
+
+  // using useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // fetching data
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.26920&lng=73.00900&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // Optional Chaining
+    setListOfRes(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   return (
     <div className="restaurent_container">
-      {/* <form action="">
-        <input
-          className="i_search"
-          type="search"
-          placeholder="search your fav food"
-        /> */}
       <button
         className="btn_search"
         type="submit"
@@ -25,7 +36,6 @@ const RestaurentList = () => {
       >
         Filter Restaurant
       </button>
-      {/* </form> */}
       <h1 className="restaurent_heading">Top restaurant chains in Jodhpur</h1>
       <section className="restaurent_items">
         {listOfRes.map((res, index) => {
